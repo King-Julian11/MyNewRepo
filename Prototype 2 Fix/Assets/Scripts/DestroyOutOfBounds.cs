2a59908c58e5898c7b37c89fe580c325864cb12f
King-Julian11 <blazerman741@gmail.com>
2022-02-05 13:24:34 -0600

    Prototype 2 changes

---
+++ Prototype 2 Fix/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottombound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Separating the food from the animals going out of bounds
+        //Food goes out of bounds
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        //Animals go out of bounds
+        if (transform.position.z < bottombound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
