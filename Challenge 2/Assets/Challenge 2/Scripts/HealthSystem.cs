7a53c5b7464c2a27a8f5bc87c5294f9b0633d650
King-Julian11 <blazerman741@gmail.com>
2022-02-06 18:29:26 -0600

    Challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/HealthSystem.cs
@@ -0,0 +1,98 @@
+﻿//This script is based on https://www.youtube.com/watch?v=3uyolYVsiWc
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public List<Image> hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    public bool gameOver = false;
+    public bool won = false;
+
+    public GameObject gameOverText;
+
+    private DisplayScore scoreScript;
+
+    private void Start()
+    {
+        scoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+    void Update()
+    {
+        //If health is somehow more than max health, set health to max health
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        if (scoreScript.score >= 5)
+        {
+            won = true;
+            gameOver = true;
+            gameOverText.SetActive(true);
+
+            //Press R to restart if game is over
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
+        }
+
+
+        for (int i = 0; i < hearts.Count; i++)
+        {
+            //Display full or empty heart sprite based on current health
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            //Show the number of hearts equal to current max health
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        if (health <= 0)
+        {
+            gameOver = true;
+            gameOverText.SetActive(true);
+
+            //Press R to restart if game is over
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+    }
+
+    public void AddMaxHealth()
+    {
+        maxHealth++;
+    }
+
+
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 3.0f;
+    private float spawnInterval = 5.0f;
+
+    public HealthSystem healthSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSystem = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnRandomPrefab());
+    }
+
+    IEnumerator SpawnRandomPrefab()
+    {
+        yield return new WaitForSeconds(spawnInterval);
+
+        while (!healthSystem.gameOver)
+        {
+            SpawnRandomBall();
+
+            float randomDelay = Random.Range(startDelay, spawnInterval);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int ballNumber = Random.Range(0, ballPrefabs.Length);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballNumber], spawnPos, ballPrefabs[ballNumber].transform.rotation);
+    }
+
+}
