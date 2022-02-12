b14795ce0f9b118293feaf7af7e628f36a965f23
King-Julian11 <blazerman741@gmail.com>
2022-02-12 17:21:02 -0600

    Prototype 3 Part 1

---
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30f;
+    private PlayerController playerControllerScript;
+
+    private float leftBound = -15;
+
+    private void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
