c74f7f11a5c1755518192ae5efdff257397545ad
King-Julian11 <blazerman741@gmail.com>
2022-03-06 23:30:15 -0600

    Prototype 5A

---
+++ PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -0,0 +1,52 @@
+﻿/* Julian Salgado
+ * Assignment 5A
+ * Controls gem behavior
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehaviour : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public GameObject collectedParticleSystem;
+	public CircleCollider2D gemCollider2D;
+
+	private float durationOfCollectedParticleSystem;
+
+	private UIManager uiManager;
+
+	private bool triggered;
+
+
+	void Start()
+	{
+		uiManager = GameObject.FindObjectOfType<UIManager>();
+		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+	}
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player") && !triggered) {
+			GemCollected ();
+			triggered = true;
+			uiManager.score++;
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gemVisuals.SetActive (false);
+		collectedParticleSystem.SetActive (true);
+		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+
+	}
+
+	void DeactivateGemGameObject()
+	{
+		gameObject.SetActive (false);
+	}
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,58 @@
+﻿/* Julian Salgado
+ * Assignment 5A
+ * Controls score, win condiitons, lose conditions, and restarting
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public PlayerPlatformerController playerScript;
+
+    public bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (scoreText == null)
+        {
+            scoreText = FindObjectOfType<Text>();
+        }
+
+        if (playerScript == null)
+        {
+            playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPlatformerController>();
+        }
+
+        scoreText.text = "Collect 10 gems\nScore: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerScript.gameOver)
+        {
+            scoreText.text = "Collect 10 gems\nScore: " + score;
+        }
+
+        if (score >= 10)
+        {
+            playerScript.gameOver = true;
+            won = true;
+
+            scoreText.text = "You win!\nPress R to Try Again!";
+        }
+
+        if (playerScript.gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
