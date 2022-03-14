8bdea47f1652bb218bd0242723e9b1a475ccace1
King-Julian11 <blazerman741@gmail.com>
2022-03-13 23:08:00 -0500

    Prototype 5B Level

---
+++ 3DProtoype/Assets/MyFirstPersonController/Scripts/TriggerZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerZone : MonoBehaviour
+{
+    private bool triggered = false;
+    private bool won = false;
+    public Text textbox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            won = true;
+        }
+
+        if (won)
+        {
+            textbox.text = "You win!";
+        }
+    }
+}
