1d6736a3942c5bf197213613ce942c8055001f0a
King-Julian11 <blazerman741@gmail.com>
2022-03-23 23:30:54 -0500

    Assignment 6

---
+++ OOPinUnity/Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField]private InventoryItem item;
+
+        // Use this for initialization
+        void Start()
+        {
+            item = new InventoryItem();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
