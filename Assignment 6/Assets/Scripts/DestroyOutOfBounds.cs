c17707965072140c0890a4924a70b0ecc3cf0db3
King-Julian11 <blazerman741@gmail.com>
2022-04-03 13:46:37 -0500

    Assignment 6

---
+++ Assignment 6/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottombound = -10;
+
+    private HealthSystem healthSystemScript;
+
+    private void Start()
+    {
+        healthSystemScript = GameObject.FindGameObjectWithTag("HealthSystem").GetComponent<HealthSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Separating the food from the animals going out of bounds
+        //Food goes out of bounds
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        //Animals go out of bounds
+        if (transform.position.z < bottombound)
+        {
+            //Debug.Log("Game Over!");
+
+            healthSystemScript.TakeDamage();
+
+            Destroy(gameObject);
+        }
+    }
+}
